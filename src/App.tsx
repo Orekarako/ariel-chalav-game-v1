1f53e98a60884cdef92ea8c44d73b663eefb62b5
Orekarako <orekarako@gmail.com>
2025-02-21 18:11:33 +0200

    שגדשדג

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [magicWord, setMagicWord] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const correctMagicWord = "ראובן המלאך , אוראל הנסיך , גסיקה הפלצנית , רוברוברוב תפתח"; // המילה הנכונה
+  const correctMagicWord = "ראובן המלאך" 
 
   const buttons = ["Fuck", "Marry", "Kill"];
 
